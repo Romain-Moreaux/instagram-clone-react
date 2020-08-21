e6e1ccd03b5434fd9c8a4a51ab2ab575c23c4f1f
Romain Moreaux <romainmoreaux@MacBook-Pro.local>
2020-08-21 20:30:48 +0200

    added Grid and Container

--- src/components/Aside.js
+++ src/components/Aside.js
@@ -1,15 +1,12 @@
 import React from 'react'
-import { makeStyles } from '@material-ui/core'
+import { makeStyles, Grid } from '@material-ui/core'
 import PropTypes from 'prop-types'
 import InstagramEmbed from 'react-instagram-embed'
 
 const useStyles = makeStyles({
-  aside: ({ position }) =>
-    position === 'right'
-      ? {
-          marginLeft: '20px',
-        }
-      : { marginRight: '20px' },
+  aside: {
+    paddingLeft: '24px',
+  },
 })
 
 function Aside(props) {
@@ -17,20 +14,22 @@ function Aside(props) {
   const classes = useStyles(props)
 
   return (
-    <div className={classes.aside}>
-      <InstagramEmbed
-        url="https://instagr.am/p/Zw9o4/"
-        maxWidth={320}
-        hideCaption={false}
-        containerTagName="div"
-        protocol=""
-        injectScript
-        onLoading={() => {}}
-        onSuccess={() => {}}
-        onAfterRender={() => {}}
-        onFailure={() => {}}
-      />
-    </div>
+    <Grid
+      container
+      item
+      className={classes.aside}
+      xs={12}
+      md={5}
+      direction="column"
+      alignItems="center"
+    >
+      <p>Follower 1</p>
+      <p>Follower 2</p>
+      <p>Follower 3</p>
+      <p>Follower 4</p>
+      <p>Follower 5</p>
+      <p>Follower 6</p>
+    </Grid>
   )
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,35 +1,36 @@
 import React from 'react'
-import { Avatar, makeStyles } from '@material-ui/core'
+import { Avatar, makeStyles, Grid, Container } from '@material-ui/core'
 import avatarImg from '../images/avatar1.jpg'
 import InstaLogo from '../images/logo_insta.png'
 import { auth } from '../init-firebase'
 
 const useStyles = makeStyles({
   header: {
-    height: '60px',
+    // maxHeight: '60px',
     backgroundColor: 'white',
-    padding: '10px',
+    padding: '12px 0',
     borderBottom: '1px solid lightgray',
-    display: 'flex',
-    justifyContent: 'space-between',
+    // display: 'flex',
+    // justifyContent: 'space-between',
     position: 'sticky',
     top: 0,
     zIndex: 1,
   },
   logo: {
-    display: 'flex',
+    // display: 'flex',
   },
 
   image: {
     objectFit: 'contain',
     objectPosition: 'left',
+    height: '29px',
   },
 
   button: {
     backgroundColor: 'transparent',
     border: 0,
     '&:first-of-type': {
-      marginLeft: 'auto',
+      // marginLeft: 'auto',
       marginRight: '10px',
     },
   },
@@ -44,41 +45,47 @@ function Header({ user, setOpenSignUp, setOpenSignIn }) {
   const classes = useStyles()
 
   return (
-    <div className={classes.header}>
-      <div className={classes.logo}>
-        <img src={InstaLogo} alt="" className={classes.image} />
-      </div>
-      {user ? (
-        <>
-          <button
-            className={`${classes.button}`}
-            onClick={() => auth.signOut()}
-          >
-            Logout
-          </button>
-          <Avatar
-            src={avatarImg}
-            alt="Romain Moreaux"
-            className={classes.user}
-          />
-        </>
-      ) : (
-        <>
-          <button
-            className={`${classes.button}`}
-            onClick={() => setOpenSignUp(true)}
-          >
-            Sign up
-          </button>
-          <button
-            className={`${classes.button}`}
-            onClick={() => setOpenSignIn(true)}
-          >
-            Sign In
-          </button>
-        </>
-      )}
-    </div>
+    <header className={classes.header}>
+      <Container maxWidth="md">
+        <Grid container alignItems="center">
+          <Grid item className={classes.logo} xs={4}>
+            <img src={InstaLogo} alt="" className={classes.image} />
+          </Grid>
+          <Grid container item xs={8} justify="flex-end">
+            {user ? (
+              <>
+                <button
+                  className={`${classes.button}`}
+                  onClick={() => auth.signOut()}
+                >
+                  Logout
+                </button>
+                <Avatar
+                  src={avatarImg}
+                  alt="Romain Moreaux"
+                  className={classes.user}
+                />
+              </>
+            ) : (
+              <>
+                <button
+                  className={`${classes.button}`}
+                  onClick={() => setOpenSignUp(true)}
+                >
+                  Sign up
+                </button>
+                <button
+                  className={`${classes.button}`}
+                  onClick={() => setOpenSignIn(true)}
+                >
+                  Sign In
+                </button>
+              </>
+            )}
+          </Grid>
+        </Grid>
+      </Container>
+    </header>
   )
 }
 
